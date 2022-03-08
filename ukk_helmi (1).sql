47ab60fec42bda03ea82d952356049e58b916331
makmugaming <helmiar527@gmail.com>
2022-03-08 14:07:23 +0700

    update database

---
+++ ukk_helmi (1).sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 08 Mar 2022 pada 08.06
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 7.3.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ukk_helmi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `id_pengguna` int(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pengguna`
+--
+
+INSERT INTO `pengguna` (`id_pengguna`, `email`, `nik`, `nama_lengkap`, `password`) VALUES
+(2, 'helmiar527@gmail.com', '3579022705040001', 'Helmi Ainur Rofiq', '0192023a7bbd73250516f069df18b500');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `perjalanan`
+--
+
+CREATE TABLE `perjalanan` (
+  `id_perjalanan` int(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam` varchar(10) NOT NULL,
+  `lokasi` varchar(50) NOT NULL,
+  `suhu` varchar(20) NOT NULL,
+  `nik` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `perjalanan`
+--
+
+INSERT INTO `perjalanan` (`id_perjalanan`, `tanggal`, `jam`, `lokasi`, `suhu`, `nik`) VALUES
+(1, '2022-03-01', '8.00', 'Batu', '35', '35795648531648001'),
+(2, '2022-03-02', '8.10', 'Kastubi', '34', '35795648531648002'),
+(3, '2022-03-04', '8.30', 'Malang', '36', '35795648531648003'),
+(4, '2022-03-03', '8.20', 'Magelang', '36,6', '35795648531648004'),
+(5, '2022-03-05', '8.40', 'Jogja', '35,4', '35795648531648005'),
+(6, '2022-03-06', '8.50', 'Cibubur', '34,4', '35795648531648006'),
+(7, '2022-03-07', '8.32', 'Cikarang', '31,4', '35795648531648007'),
+(8, '2022-03-08', '8.56', 'Tegal', '35,6', '35795648531648008'),
+(9, '2022-03-09', '8.15', 'Sragen', '33,9', '35795648531648009'),
+(10, '2022-03-10', '8.25', 'Junrejo', '34,2', '35795648531648010'),
+(11, '2022-03-31', '9.35', 'Lumajang', '35,6', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id_pengguna`);
+
+--
+-- Indeks untuk tabel `perjalanan`
+--
+ALTER TABLE `perjalanan`
+  ADD PRIMARY KEY (`id_perjalanan`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  MODIFY `id_pengguna` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `perjalanan`
+--
+ALTER TABLE `perjalanan`
+  MODIFY `id_perjalanan` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
